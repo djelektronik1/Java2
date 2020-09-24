c19119f6df2d7286209db41e328d1aa436cfa893
djelektronik1 <56953131+djelektronik1@users.noreply.github.com>
2020-09-24 22:16:01 +0300

    Add files via upload

---
+++ Idz 1 классы/src/Klinika/Runner.java
@@ -0,0 +1,68 @@
+package Klinika;
+
+import Klinika.Bolnie;
+import Klinika.Sort;
+
+public class Runner {
+	
+	
+	public static void main(String[] args) {
+
+        Bolnie[] Pacient = new Bolnie[10];
+
+        Pacient[0] = new Bolnie("Дмитрий", "Имануилов", "Алексеевич","13 БССР 23","+37529889955",1, "angina");
+        Pacient[1] = new Bolnie("Василий", "Тарасенко", "Петрович","jyjhg СР 23","+37569632147",2, "uxo");
+        Pacient[2] = new Bolnie("Алесей", "Колесник", "Андреевич","Гомель 646","+89562310568",3, "grip");
+        Pacient[3] = new Bolnie("Дмитрий", "Турецкий", "Федорович","GHG ryh45","+37656565678",4, "Zdorov");
+        Pacient[4] = new Bolnie("Семён", "Семёновч", "Шпат","Кукухино 76","+79856215850",5, "COVID-19");
+        Pacient[5] = new Bolnie("Иван", "Васильевич", "Федосеко","Четыре хватит","+37529999998",6, "ne noga");
+        Pacient[6] = new Bolnie("Измаил", "Николаевич", "Кукухин","Пожалуйста","+37555325684",7, "zdorov");
+        Pacient[7] = new Bolnie("Павел", "Зачётов", "Припоев","В зачётку","+37529889966",8, "angina");
+        Pacient[8] = new Bolnie("Алексей", "Безымянный", "Никакоев","0 лет БССР 23","+12345678912",9, "ne zdorov");
+        Pacient[9] = new Bolnie("Семён", "Семёнtовч", "Шпат","Куtкуино 76","+79862158450",10,"Noga");
+
+        
+
+        // Sort
+        System.out.println("");
+        System.out.println("Sorting by Names");
+        System.out.println("");
+        for( Bolnie bolnie: Sort.byNames(Pacient)){
+            if (bolnie != null){
+            	bolnie.show();
+            }
+        }
+                
+        // sort in range
+        
+        int min_ = 3;
+        int max_ = 8;
+        
+        System.out.println("");
+        System.out.println("Sorting by Range");
+        System.out.println("");
+        for( Bolnie bolnie: (Pacient)){
+            if (bolnie != null && bolnie.getMedical_card_number() > min_ && bolnie.getMedical_card_number() < max_){
+            	bolnie.show();
+            }
+        }
+        
+        // sort in Disease
+        
+        String disease_ = "angina";
+        
+        System.out.println("");
+        System.out.println("Sorting by Disease");
+        System.out.println("");
+        for(Bolnie bolnie:(Pacient)){
+            if (bolnie != null && bolnie.getDisease().equals(disease_)){
+            	bolnie.show();
+            }
+        }
+        
+        
+        
+        }
+    }
+
+
---
+++ Idz 1 классы/src/Klinika/Sort.java
@@ -0,0 +1,15 @@
+package Klinika;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Sort {
+	
+	public static Bolnie[] byNames(Bolnie[] array){
+        Arrays.sort(array, Comparator.comparing(Bolnie::getName));
+        return array;
+    }
+
+   
+
+}
