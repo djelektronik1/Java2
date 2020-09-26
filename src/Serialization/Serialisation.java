3afceecbe358c43424577b0423bc8536a2cd4af1
djelektronik1 <56953131+djelektronik1@users.noreply.github.com>
2020-09-26 18:48:51 +0300

    Add files via upload

---
+++ src/Serialization/Serialisation.java
@@ -0,0 +1,39 @@
+package Serialization;
+
+import Serialization.Bolnie;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.file.NoSuchFileException;
+
+
+public class Serialisation {
+	
+	public void serialise(Bolnie[] bolnie)  {
+		
+        ObjectOutputStream objectOutputStream = null;
+        
+        if (bolnie.length == 0);
+        {
+        
+        	try 
+        		{        	
+        		   
+        		   objectOutputStream = new ObjectOutputStream(new FileOutputStream("src/bolnie.ser"));
+        		   objectOutputStream.writeObject(bolnie);
+        		   objectOutputStream.close();            
+        		} 
+        
+        		catch (NoSuchFileException ex)
+                {
+        			System.out.println("File not found");
+                } 
+        
+        			catch (IOException e) 
+        			{
+        			    System.out.println(e);
+        			}
+    }
+
+}
+}
